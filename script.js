ffc84c5cad9ecfcd6c95b04542662b536f54eb7e
coleb <cole.ballard97@gmail.com>
2021-01-14 13:55:54 -0600

    fixed api calling, and changed HTML layout and styling

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+$(document).ready(function() {
 
+  $(document).foundation();
+
+  var genre;
+  $('.genre').on('click',function(){
+
+    var G = Math.floor(Math.random() * 20);
+    var P = Math.floor(Math.random() * 100);
+
+    genre = ($(this).text()).toLowerCase().split(' ').join('-');
+
+    if (genre.localeCompare('rpg') == 0)
+      genre = 'role-playing-games-rpg';
+      
+    const settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://rawg-video-games-database.p.rapidapi.com/games?genres=" + genre + "&page=" + P,
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "16f6042a71mshd7819557aed7c23p19670cjsn3b86dc1f29bc",
+        "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com"
+      }
+    }; $.ajax(settings).done(function (response) {
+    console.log(response);
+    var randomGame = response.results[G];
+    console.log(randomGame);
+    var gameName = randomGame.name;
+    console.log(gameName);
+      
+    });
+
+  });
+
+});
